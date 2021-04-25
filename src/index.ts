9d91fd0ef47d12fad8c85a1fe9e2e6e1ea3466b6
Ben Kelly <b-kelly@users.noreply.github.com>
2021-04-24 23:16:06 -0400

    drop code implying functionality that is evading me

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,14 @@ import { gameConfig } from "./config";
 import { CommandEmitterPlugin } from "./plugins/CommandEmitterPlugin";
 import { LocalCommandEmitterPlugin } from "./plugins/LocalCommandEmitterPlugin";
 
-document.querySelector("#js-play-single-btn").addEventListener("click", () => {
-    startGame(LocalCommandEmitterPlugin);
-});
+gameConfig.plugins = {
+    global: [
+        {
+            key: CommandEmitterPlugin.name,
+            plugin: LocalCommandEmitterPlugin,
+            start: true,
+        },
+    ],
+};
 
-function startGame(plugin: typeof CommandEmitterPlugin) {
-    document.querySelector(".js-step-1-container").classList.add("d-none");
-    document.querySelector(".js-step-2-container").classList.remove("d-none");
-
-    gameConfig.plugins = {
-        global: [
-            {
-                key: CommandEmitterPlugin.name,
-                plugin: plugin,
-                start: true,
-            },
-        ],
-    };
-
-    new Phaser.Game(gameConfig);
-}
+new Phaser.Game(gameConfig);
